202bdf6a25f4848c4ef57e6f8e8a7179f070921b
MrTat-9921 <52516762+MrTat-9921@users.noreply.github.com>
2024-08-01 00:05:59 +0700

    footer

--- new/src/view/Foot.jsx
+++ new/src/view/Foot.jsx
@@ -1,8 +1,193 @@
-import { memo } from "react"
+import { Button } from "antd";
+import { memo } from "react";
 
-const Foot = ()=>{
-    return (
-        <div>Footer</div>
-    )
-}
-export default memo(Foot)
+const Foot = () => {
+  return (
+    <div>
+      <section class="d7ed-IB_g3V">
+        <div class="container flex text-center gap-16">
+          <a
+            className="basis-1/4"
+            href="https://www.sendo.vn/huong-dan/ve-sendo"
+          >
+            <img src="https://media3.scdn.vn/img4/2020/12_16/gJwXr6FFZKZCGKWaz4RB.png" />
+            <div>
+              <span class="block font-bold">Siêu nhiều hàng tốt</span>
+              <span>Cần gì cũng có 26 ngành hàng &amp; 10 triệu sản phẩm</span>
+            </div>
+          </a>
+          <a
+            className="basis-1/4"
+            href="https://www.sendo.vn/huong-dan/ve-sendo"
+          >
+            <img src="https://media3.scdn.vn/img4/2020/12_16/EfZWQVfV6nQzu2vMmnwC.png" />
+            <div>
+              <span class="block font-bold">Siêu yên tâm</span>
+              <span>Miễn phí đổi trả 48h</span>
+            </div>
+          </a>
+          <a
+            className="basis-1/4"
+            href="https://www.sendo.vn/huong-dan/ve-sendo"
+          >
+            <img src="https://media3.scdn.vn/img4/2020/12_16/j5C6IQz7gIXPgjFJxmRz.png" />
+            <div>
+              <span class="block font-bold">Siêu tiện lợi</span>
+              <span>Mang thế giới mua sắm của Sendo trong tầm tay bạn</span>
+            </div>
+          </a>
+          <a
+            className="basis-1/4"
+            href="https://www.sendo.vn/huong-dan/ve-sendo"
+          >
+            <img src="https://media3.scdn.vn/img4/2020/12_16/7AJFQGQ5qvS7gGOz8P7a.png" />
+            <div>
+              <span class="block font-bold">Siêu tiết kiệm</span>
+              <span>
+                Giá hợp lý, vừa túi tiền. Luôn có nhiều chương trình khuyến mãi
+              </span>
+            </div>
+          </a>
+        </div>
+      </section>
+      <section class="bg-slate-100 py-5">
+        <div class="container flex ">
+          <div class="basis-1/4 flex flex-col">
+            <span class="font-bold">VỀ CHÚNG TÔI</span>
+            <a href="https://www.sendo.vn/huong-dan/ve-sendo?ref=footer">
+              Giới thiệu Sendo.vn
+            </a>
+            <a href="https://help.sendo.vn/hc/vi/articles/360059470652-L%E1%BB%A3i-%C3%ADch-c%E1%BB%A7a-mua-h%C3%A0ng-Shop-SenMall-l%C3%A0-g%C3%AC-?ref=footer">
+              Giới thiệu SenMall
+            </a>
+            <a href="https://www.sendo.vn/quy-che-hoat-dong?ref=footer">
+              Quy chế hoạt động
+            </a>
+            <a href="https://www.sendo.vn/chinh-sach-bao-mat?ref=footer">
+              Chính sách bảo mật
+            </a>
+            <a href="https://ban.sendo.vn/chinh-sach-nguoi-ban/49-5-quy-dinh-van-chuyen/175-phu-luc-09-quy-dinh-lay-hang%2C-giao-hang%2C-tra-hang%2C-thu-hoi-danh-cho-don-vi-van-chuyen">
+              Giao hàng và Nhận hàng
+            </a>
+            <a href="https://www.sendo.vn/huong-dan/dieu-khoan-su-dung?ref=footer">
+              Điều khoản sử dụng
+            </a>
+          </div>
+          <div class="basis-1/4 flex flex-col">
+            <span class="font-bold">DÀNH CHO NGƯỜI MUA</span>
+            <a href="https://help.sendo.vn/hc/vi/articles/360059468452?ref=footer">
+              Giải quyết khiếu nại
+            </a>
+            <a href="https://help.sendo.vn/hc/vi/articles/360059885831?ref=footer">
+              Hướng dẫn mua hàng
+            </a>
+            <a href="https://help.sendo.vn/hc/vi/articles/360059893871?ref=footer">
+              Chính sách đổi trả
+            </a>
+            <a href="https://help.sendo.vn/hc/vi?ref=footer">
+              Chăm sóc khách hàng
+            </a>
+            <a href="https://www.sendo.vn/tien-ich/nap-tien?ref=footer">
+              Nạp tiền điện thoại
+            </a>
+          </div>
+          <div class="basis-1/4 flex flex-col">
+            <span class="font-bold">DÀNH CHO NGƯỜI BÁN</span>
+            <a href="https://ban.sendo.vn/gioi-thieu-trang-ban?ref=footer">
+              Quy định đối với người bán
+            </a>
+            <a href="https://ban.sendo.vn/chinh-sach-nguoi-ban?ref=footer">
+              Chính sách bán hàng
+            </a>
+            <a href="https://ban.sendo.vn/chinh-sach-nguoi-ban/36-5-quy-dinh-kiem-duyet?ref=footer">
+              Hệ thống tiêu chí kiểm duyệt
+            </a>
+            <a href="https://ban.sendo.vn/gioi-thieu-trang-ban?ref=footer">
+              Mở shop trên Sendo
+            </a>
+          </div>
+          <div class="basis-1/4 flex flex-col">
+            <span class="font-bold">TẢI ỨNG DỤNG SENDO</span>
+            <span class="a258-ynyK1p d7ed-oz4bcc d7ed-bjQW4F d7ed-mzOLVa">
+              Mang thế giới mua sắm của Sendo trong tầm tay bạn
+            </span>
+            <div class="flex flex-row flex-wrap gap-2">
+              <a href="https://go.onelink.me/app/2a94415f">
+                <img
+                  className="h-10"
+                  src="https://media3.scdn.vn/img4/2020/12_16/5lUTWdk3DXr8nlC9MDII.png"
+                />
+              </a>
+              <a href="https://go.onelink.me/app/2a19ae85">
+                <img
+                  className="h-10"
+                  src="https://media3.scdn.vn/img4/2021/10_26/0ZARLASzVrfL92924rzW.png"
+                />
+              </a>
+              <a href="https://appgallery.huawei.com/app/C101535313">
+                <img
+                  className="h-10"
+                  src="https://media3.scdn.vn/img4/2021/03_19/AMV086JNpEbm4OGAvVng.png"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="bg-neutral-900 py-5 text-white">
+        <div class="container flex">
+          <div class="basis-1/2 flex flex-col gap-2 pr-24">
+            <span class="font-bold">
+              Công ty Cổ phần Công nghệ Sen Đỏ, thành viên của Tập đoàn FPT
+            </span>
+            <span>
+              Số ĐKKD: 0312776486 - Ngày cấp: 13/05/2014, được sửa đổi lần thứ
+              20, ngày 26/04/2022.
+            </span>
+            <span>Cơ quan cấp: Sở Kế hoạch và Đầu tư TPHCM.</span>
+            <span>
+              Địa chỉ: Tầng 5, Tòa nhà A, Vườn Ươm Doanh Nghiệp, Lô D.01, Đường
+              Tân Thuận, Khu chế xuất Tân Thuận, Phường Tân Thuận Đông, Quận 7,
+              Thành phố Hồ Chí Minh, Việt Nam.
+            </span>
+            <span>
+              Email: <a href="mailto:lienhe@sendo.vn">lienhe@sendo.vn</a>
+            </span>
+            <div class="flex flex-row gap-5">
+              <a href="http://online.gov.vn/Home/WebDetails/21391">
+                <img
+                  src="https://media3.scdn.vn/img4/2020/12_16/XhpGDnvWqrlKeHLst3aS.png"
+                  class="h-10"
+                />
+              </a>
+              <a href="https://help.sendo.vn/hc/vi/articles/115001260091-L%C3%A0m-th%E1%BA%BF-n%C3%A0o-%C4%91%E1%BB%83-tr%E1%BA%A3-h%C3%A0ng">
+                <img
+                  src="https://media3.scdn.vn/img4/2020/12_16/h6lEMGIAt4Uapd0Mls34.png"
+                  class="h-10"
+                />
+              </a>
+            </div>
+          </div>
+          <div class="basis-1/2 pr-24">
+            <span class="font-bold">
+              Đăng ký nhận bản tin ưu đãi khủng từ Sendo
+            </span>
+            <form class="flex content-end gap-2">
+              <div class="grow">
+                <input
+                  placeholder="Email của bạn là"
+                  type="text"
+                  class="p-2 rounded mt-2 w-full"
+                />
+              </div>
+              <div class="self-end">
+                <input type="submit" className="bg-red-700 rounded py-2 px-10" value={"Đăng ký"} />
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+export default memo(Foot);
